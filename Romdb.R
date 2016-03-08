c1c3f19fc7f9d6f186f3d4ee86927f303770d3ee
Stan125 <stani@StaniBook.local>
2016-03-08 02:18:09 +0100

    Final working state

--- Romdb.R
+++ Romdb.R
@@ -14,65 +14,80 @@ library(dplyr)
 library(ggplot2)
 
 # Do a search of the series/movie
-search_results <- search_by_title("Suits", type = "series")
+search_results <- search_by_title("The Office", type = "series")
 
 # Specify here which of the results is the right one
 title <- search_results[1, 1]
 
 # Make containers
-suits <- matrix(ncol = 3)
-ergebnis <- c()
+object <- matrix(ncol = 3)
+result <- matrix(nrow = 1)
 
 # Starting values
-j <- 1
+i <- 1
 # Pull data
-for (i in 1:6) {
+while (nrow(result) == 1) {
+  if (i > 1 & j == 1) {
+    break
+  }
+  # Starting values
   j <- 1
-  while (nrow(ergebnis) == 1) {
-    
-    # Get data from OMDB, suppress error
-    ergebnis <- title %>% 
+  
+  while (nrow(result) == 1) {
+    # Get data from OMDB
+    result <- title %>% 
           as.character() %>%
           find_by_title(.,
                         season = i,
                         episode = j)
     
-    # IMDBRating rausziehen
-    ergebnis <- ergebnis %>% dplyr::select(imdbRating)
+    # Next season if no more episodes
+    if (nrow(result) != 1) {
+      result <- matrix(nrow = 1)
+      break
+    }
     
-    # Dataframe füllen
-    suits <- rbind(suits,
-                   c(i, j, ergebnis))
+    # Get IMDBRating
+    result <- result %>% dplyr::select(imdbRating)
+    
+    # Fill container
+    object <- rbind(object,
+                   c(i, j, result))
     
     # Next iteration
     j <- j + 1
-  } 
+  }
+  print(paste("Last episode of Season", i, "was episode", j - 1))
+  i <- i + 1
 }
 
-suits_clean <- data.frame(suits[2:nrow(suits), ])
-colnames(suits_clean) <- c("Season", "Episode", "IMDBRating")
-suits_clean$episode.no <- 1:nrow(suits_clean)
-suits_clean$Season <- as.numeric(suits_clean$Season) %>% as.factor()
-suits_clean$Episode <- as.numeric(suits_clean$Episode) %>% as.factor()
-suits_clean$IMDBRating <- as.numeric(suits_clean$IMDBRating)
-suits_clean <- as.data.frame(suits_clean)
+# Make DF
+object <- data.frame(object[2:nrow(object), ])
 
+# Rename columns
+colnames(object) <- c("Season", "Episode", "IMDBRating")
+
+# Necessary transformations to new df
+object_clean <- data.frame(object)
+object_clean$episode.no <- 1:nrow(object_clean)
+object_clean$Season <- as.numeric(object$Season) %>% as.factor()
+object_clean$Episode <- as.numeric(object$Episode) %>% as.factor()
+object_clean$IMDBRating <- as.numeric(object$IMDBRating)
 
 # Manual adjustments
-suits_clean[70, 3] <- 9.9
-suits_clean[68, 3] <- 9.4
+
 
 # Graph
-png("suitsIMDB.png",
+png(paste0(title,"IMDB.png"),
     res = 125,
     height = 900,
     width = 1600)
-ggplot(data = suits_clean, aes(x = episode.no,
-                         y = IMDBRating,
-                         col = Season)) +
+ggplot(data = object_clean, aes(x = episode.no,
+                                y = IMDBRating,
+                                col = Season)) +
   geom_point() +
   stat_smooth(method = "lm") +
-  ggtitle("IMDB Ratings of Suits episodes \n") +
+  ggtitle(paste0("IMDB Ratings of ", title, " episodes \n")) +
   theme_bw() +
   xlab("Episode No.")
 dev.off()
