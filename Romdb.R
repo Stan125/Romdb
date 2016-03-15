8c0aeb00e3c1e548499f2c7a59a8a83d225e1a03
Stan125 <stanislaus@stadlmann.cm>
2016-03-15 13:06:07 +0100

    Build upon httr
    - less packages
    - but faster

--- Romdb.R
+++ Romdb.R
@@ -8,16 +8,12 @@
 # devtools::install_github("hrbrmstr/omdbapi")
 
 # Packages
-library(XML)
-library(omdbapi)
+library(httr)
 library(dplyr)
 library(ggplot2)
 
-# Do a search of the series/movie
-search_results <- search_by_title("Scrubs", type = "series")
-
-# Specify here which of the results is the right one
-title <- search_results[1, 1]
+# Specify name of Series
+title <- "Game of Thrones"
 
 # Make containers
 object <- matrix(ncol = 3)
@@ -27,30 +23,37 @@ result <- matrix(nrow = 1)
 i <- 1
 j <- 1
 # Pull data
-while (nrow(result) == 1) {
+while (!is.null(result)) {
   if (i > 1 & j == 1) {
     break
   }
   # Starting values
   j <- 1
   
-  while (nrow(result) == 1) {
+  while (!is.null(result)) {
     # Get data from OMDB
-    result <- title %>% 
-          as.character() %>%
-          find_by_title(.,
-                        season = i,
-                        episode = j)
+    query <- GET("http://www.omdbapi.com/", 
+                  query = list(t = title, 
+                               Season = i,
+                               Episode = j),
+                 r = "json")
+    
+    # Parse content
+    query_parsed <- content(query, as = "parsed")
+    
+    # Store content
+    result <- query_parsed$imdbRating
+    
+    if (!is.null(result) && result == "N/A") {
+      result <- NA
+    }
     
     # Next season if no more episodes
-    if (nrow(result) != 1) {
+    if (is.null(result)) {
       result <- matrix(nrow = 1)
       break
     }
-    
-    # Get IMDBRating
-    result <- result %>% dplyr::select(imdbRating)
-    
+
     # Fill container
     object <- rbind(object,
                    c(i, j, result))
@@ -73,15 +76,23 @@ object_clean <- data.frame(object)
 object_clean$episode.no <- 1:nrow(object_clean)
 object_clean$Season <- as.numeric(object$Season) %>% as.factor()
 object_clean$Episode <- as.numeric(object$Episode) %>% as.factor()
-object_clean$IMDBRating <- as.numeric(object$IMDBRating)
+object_clean$IMDBRating <- as.numeric(as.character(object$IMDBRating))
+
+# Delete Season if only NA's
+for (season in unique(object_clean$Season)) {
+  if (all(is.na(object_clean[object_clean$Season == season, "IMDBRating"]))) {
+    object_clean <- object_clean[object_clean$Season != season, ]
+  }
+}
+
 
 # Manual adjustments
 
 
 # Graph
 ggplot(data = object_clean, aes(x = episode.no,
-                                y = IMDBRating,
-                                col = Season)) +
+                          y = IMDBRating,
+                          col = Season)) +
   geom_point() +
   stat_smooth(method = "lm") +
   ggtitle(paste0("IMDB Ratings of ", title, " episodes \n")) +
